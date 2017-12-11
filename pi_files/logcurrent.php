127cfa6990c822aa7e404aa686e526d7cc071f77
Jasper <jasper@jaspersijgers.nl>
2017-12-11 14:29:58 +0100

    Updated logcurrent.php to the latest version

--- pi_files/logcurrent.php
+++ pi_files/logcurrent.php
@@ -3,20 +3,20 @@
 exec("/usr/bin/python /home/pi/reader.py", $output);
 
 // Database Values
-$vebr_laag;
-$vebr_hoog;
+$verbr_laag;
+$verbr_hoog;
 $terug_laag;
 $terug_hoog;
-$vebr_act;
+$verbr_act;
 $terug_act;
 $gasstand;
 
 foreach($output as $line){
     if(strpos($line, ':1.8.1') !== false){
-        $vebr_laag = str_replace('*kWh)', '', explode('(', $line)[1]);
+        $verbr_laag = str_replace('*kWh)', '', explode('(', $line)[1]);
     }
     elseif(strpos($line, ':2.8.1') !== false){
-        $vebr_hoog = str_replace('*kWh)', '', explode('(', $line)[1]);
+        $verbr_hoog = str_replace('*kWh)', '', explode('(', $line)[1]);
     }
     elseif(strpos($line, ':1.8.2') !== false){
         $terug_laag = str_replace('*kWh)', '', explode('(', $line)[1]);
@@ -25,7 +25,7 @@ foreach($output as $line){
         $terug_hoog = str_replace('*kWh)', '', explode('(', $line)[1]);
     }
     elseif(strpos($line, ':1.7.0') !== false){
-        $vebr_act = str_replace('*kW)', '', explode('(', $line)[1]);
+        $verbr_act = str_replace('*kW)', '', explode('(', $line)[1]);
     }
     elseif(strpos($line, ':2.7.0') !== false){
         $terug_act = str_replace('*kW)', '', explode('(', $line)[1]);
@@ -40,14 +40,14 @@ if ($mysqli->connect_errno) {
     exit;
 }
 
-$sql = "INSERT INTO `smartmeter`.`data` (`vebr_laag`, `vebr_hoog`, `terug_laag`, `terug_hoog`, `vebr_act`, `terug_act`, `gasstand`) VALUES ('$vebr_laag', '$vebr_hoog', '$terug_laag', '$terug_hoog', '$vebr_act', '$terug_act', '$gasstand');";
+$sql = "INSERT INTO `smartmeter`.`data` (`verbr_laag`, `verbr_hoog`, `terug_laag`, `terug_hoog`, `verbr_act`, `terug_act`, `gasstand`) VALUES ('$verbr_laag', '$verbr_hoog', '$terug_laag', '$terug_hoog', '$verbr_act', '$terug_act', '$gasstand');";
 
 if ($mysqli->query($sql) === TRUE) {
-    echo 'VEBR_LAAG: ' . $vebr_laag .
-        '<br>VEBR_HOOG: ' . $vebr_hoog .
+    echo 'VERBR_LAAG: ' . $verbr_laag .
+        '<br>VERBR_HOOG: ' . $verbr_hoog .
         '<br>TERUG_LAAG: ' . $terug_laag .
         '<br>TERUG_HOOG: ' . $terug_hoog .
-        '<br>VEBR_ACT: ' . $vebr_act .
+        '<br>VERBR_ACT: ' . $verbr_act .
         '<br>TERUG_ACT: ' . $terug_act .
         '<br>GASSTAND: ' . $gasstand;
     echo '<br><b>inserted successfully</b>';
